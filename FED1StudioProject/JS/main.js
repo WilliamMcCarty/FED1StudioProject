be6308ba5c5d86621eb922a81543bb7027492bc7
William McCarty <kcchief@yahoo.com>
2021-08-04 18:02:48 -0500

    Added the lightbox to the team page

---
+++ FED1StudioProject/JS/main.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    
+    //Lightbox
+    $(".thumb").on("click", function () {
+
+        var imgSrc = $(this).attr("src");
+
+        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive"/>');
+
+        $("#lightbox-container").fadeIn(500);
+
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+    });
+
+});
